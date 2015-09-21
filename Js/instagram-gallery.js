c372d35d868f612f18def34b6a3106c6dbbc5264
Jason Cameron <kiocam246@gmail.com>
2015-09-21 10:59:34 -0400

    update

--- Js/instagram-gallery.js
+++ Js/instagram-gallery.js
@@ -1,11 +1,18 @@
 
+var instagramBtn = document.createElement("button");
+instagramBtn.id = "load-more";
+
 var loadButton = document.getElementById('load-more');
+
+
+
+
 var feed = new Instafeed({
   // every time we load more, run this function
   get: "user",
   userId: 214372068,
-  limit: 6,
-  resolution: "standard_resolution",
+  limit: 7,
+  resolution: "thumbnail",
   accessToken: "214372068.cf0499d.6b8811f629984ecabd8c6d8a758b631f",
   after: function() {
     // disable button if no more results to load
@@ -14,6 +21,9 @@ var feed = new Instafeed({
     }
 },
 });
+
+
+
 
 // bind the load more button
 loadButton.addEventListener('click', function() {
