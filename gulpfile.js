3e141f58e082e6e7173ff2a2d5c2286fb51e252e
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2015-10-05 17:11:29 -0400

    add node modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,84 @@
-var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
+// =======================================
+// REQUIRED 
+// =======================================
 
-// Image Task
+var gulp           = require('gulp'),
+    uglify         = require('gulp-uglify'),
+    rename         = require('gulp-rename'),
+    plumber        = require('gulp-plumber'),
+    browserSync    = require('browser-sync'),
+    reload         = browserSync.reload,
+    autoprefixer   = require('gulp-autoprefixer'),
+    sass           = require('gulp-sass');
 
-gulp.task('image', function(){
-  gulp.src('images/*')
-      .pipe(imagemin({
-        progressive: true
-      }
-      ))
-      .pipe(gulp.dest('build/images'));
+// =======================================
+// SCRIPTS
+// =======================================
+
+// gulp.task('scripts', function(){
+//   gulp.src(['js/**/*.js', '!js/**/*.min.js'])
+      // .pipe(plumber())
+//       .pipe(rename({suffix: '.min'}))
+//       .pipe(uglify())
+//       .pipe(gulp.dest('js'))
+     // .pipe(reload({stream: true}));
+
+// });
+// =======================================
+// STYLES
+// =======================================
+
+gulp.task('styles', function(){
+  gulp.src('scss/**/*.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(autoprefixer('last 2 versions'))
+      .pipe(gulp.dest('css/'))
+      .pipe(reload({stream: true}));
+
 });
+
+// =======================================
+// HTML
+// =======================================
+
+gulp.task('html', function(){
+  gulp.src('*.html')
+      .pipe(reload({stream: true}));
+
+});
+
+
+
+// =======================================
+// BROWSER-SYNC
+// =======================================
+
+gulp.task('browser-sync', function(){
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  })
+});
+
+
+
+// =======================================
+// WATCH 
+// =======================================
+
+gulp.task('watch', function(){
+  // gulp.watch('js/**/*.js', ['scripts']);
+  gulp.watch('scss/**/*.scss', ['styles']);
+  gulp.watch('*.html', ['html']);
+});
+
+
+
+// =======================================
+// DEFAULT
+// =======================================
+
+// gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('default', ['styles', 'html', 'browser-sync', 'watch']);
